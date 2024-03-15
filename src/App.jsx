717a78e55e5d04bccab329662331fbfa171e10c0
Rocky Dey <coderrocky25@gmail.com>
2024-03-16 01:13:49 +0600

    footer section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import RecipeContainer from "./components/RecipeContainer/RecipeContainer";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Banner></Banner>
       <RecipeContainer></RecipeContainer>
+      <Footer></Footer>
     </div>
   );
 }
